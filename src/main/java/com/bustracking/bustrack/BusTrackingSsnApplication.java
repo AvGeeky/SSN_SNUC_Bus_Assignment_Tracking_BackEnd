b8c7293302b08e91719644103bb6b3ac467f08f6
AvGeeky <sai.saip2005@gmail.com>
2025-10-01 20:48:32 +0530

    Valthukal Valthukal Commit

---
+++ src/main/java/com/bustracking/bustrack/BusTrackingSsnApplication.java
@@ -0,0 +1,13 @@
+package com.bustracking.bustrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTrackingSsnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTrackingSsnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bustracking/bustrack/BusTrackingSsnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bustracking.bustrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTrackingSsnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
